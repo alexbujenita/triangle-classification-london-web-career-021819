491db7e46f9218f5ed17fe9ff375400e93596535
Alex Bujenita <dekc89@gmail.com>
2019-02-23 17:17:18 +0000

    done, tests not working with rescue

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :base, :b, :c
+
+  def initialize(base,b,c)
+    @base = base
+    @b = b
+    @c = c
+  end
+
+  def kind
+    values = [@base, @b, @c]
+    max = values.delete_at(values.index(values.max))
+    if values.sum <= max
+      begin
+        raise TriangleError
+      # rescue TriangleError => err
+      #    err.print_error
+      end
+    else
+      return :equilateral if (@base == @b) && (@b == @c)
+      return :isosceles if (@base == @b) || (@base == @c) || (@b == @c)
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def print_error
+      "Triangle not valid"
+    end
+  end
 end
